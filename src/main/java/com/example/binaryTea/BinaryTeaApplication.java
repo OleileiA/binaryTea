e61f5411df2bfb2fafe1dce20842af011b8bdfd0
cyw <18604469853@163.com>
2024-05-27 21:16:43 +0800

    feat: init

---
+++ src/main/java/com/example/binaryTea/BinaryTeaApplication.java
@@ -0,0 +1,13 @@
+package com.example.binaryTea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaryTeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaryTeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/binaryTea/BinaryTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binaryTea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaryTeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
